c70358dc900bd447a3ae6584db528c9e573441c4
Suman <tht.suman@gmail.com>
2019-10-20 12:24:07 +0530

    Updates

---
+++ precache-manifest.99e61eeca73a43237e7f18b238b406f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b377ad969fb13bc02a23c25794b42b00",
+    "url": "/calc-react/index.html"
+  },
+  {
+    "revision": "b06e29bcf4ddf9fb4bbb",
+    "url": "/calc-react/static/css/main.566b4623.chunk.css"
+  },
+  {
+    "revision": "ddbcd6d162b09b7ded68",
+    "url": "/calc-react/static/js/2.d83e2a71.chunk.js"
+  },
+  {
+    "revision": "b06e29bcf4ddf9fb4bbb",
+    "url": "/calc-react/static/js/main.5fe31740.chunk.js"
+  },
+  {
+    "revision": "7a1a998de4e6e3a6b2aa",
+    "url": "/calc-react/static/js/runtime-main.8963323e.js"
+  }
+]);
